cab87fc85a5a47659438c73a8db828edcc19062c
Evan-Yeo <me@evanyeo.com>
2020-08-05 15:31:01 +0800

    changed root to posts#index

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root 'users#sign_in'
+  root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
