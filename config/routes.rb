7bb952f2c3b5c9aafb2c283ed1dd2a64d4053d57
Darrell Yeo <zeniethlily@gmail.com>
2020-08-05 15:02:36 +0800

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root 'users#index'
+  root 'users#sign_in'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
