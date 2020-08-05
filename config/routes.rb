f82c75cfee1176db49aa1e74bc48e77197195104
Darrell Yeo <zeniethlily@gmail.com>
2020-08-05 14:57:17 +0800

    route change

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
