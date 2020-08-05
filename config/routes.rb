5d8981ca3657cc636d9519b5b1696d886d7b4946
Darrell Yeo <zeniethlily@gmail.com>
2020-08-05 12:36:51 +0800

    created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'comment/index'
+  get 'comment/new'
+  get 'comment/create'
+  get 'comment/edit'
+  get 'comment/update'
+  get 'comment/destroy'
+  get 'comment/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
