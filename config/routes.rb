c489d045ae38cc5b3fec70069a3aeee67fdbf074
Evan-Yeo <me@evanyeo.com>
2020-08-05 12:30:41 +0800

    added post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/edit'
+  get 'posts/show'
+  get 'posts/update'
+  get 'posts/destroy'
+  get 'posts/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
