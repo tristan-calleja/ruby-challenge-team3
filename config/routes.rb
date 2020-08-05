2cf415dce8a14a7394b4e36b945c6827d2468b29
Darrell Yeo <zeniethlily@gmail.com>
2020-08-05 14:32:37 +0800

    fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources: users do
-    resources: posts
+  resources :users do
+    resources :posts
   end
   
-  resources: posts do
-    resources: comments
+  resources :posts do
+    resources :comments
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
