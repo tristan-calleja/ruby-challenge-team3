206b225a4b7b7706c4b3a7aadf852c95ac1b0ca6
Darrell Yeo <zeniethlily@gmail.com>
2020-08-06 12:02:30 +0800

    post index fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+   
+
     protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :username, :phone, :address, :city, :country])
     end
 
+    def after_sign_in_path_for(resource)
+      stored_location_for(resource) || "/users/#{resource.id}/posts"
+    end
+  def after_sign_up_path_for(resource)
+      stored_location_for(resource) || "/users/#{resource.id}/posts"
+  end
 end
